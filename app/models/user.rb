7350f8cd0a96f0b123c11849ae053808c81a91be
Fikerte-T <tofikertesfaye@gmail.com>
2022-04-15 18:31:37 +0300

    Add validations for Post and User models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   has_many :comments, foreign_key: :author_id, dependent: :destroy
   has_many :likes, foreign_key: :author_id, dependent: :destroy
 
+  # validations
+  validates :name, presence: true
+  validates :posts_counter, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
+
   def recent_posts
     posts.limit(3).order(created_at: :desc)
   end
