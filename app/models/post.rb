4d7b0a05cff7daeea626e81bd71bb45cfec47106
Fikerte-T <tofikertesfaye@gmail.com>
2022-04-15 20:13:22 +0300

    Add unit specs for post, comment, and like model methods and validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,6 @@ class Post < ApplicationRecord
   validates :comments_counter, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
   validates :likes_counter, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 0}
 
-
   def recent_comments
     comments.limit(5).order(created_at: :desc)
   end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'should test model validations' do
+    user = User.create(name: 'Fikerte', bio: 'Developer')
+    subject {Post.new(user: user, title: 'Post', text: 'This is post 1', comments_counter: 6, likes_counter: 4)}
+    
+    
+    before {subject.save}
+
+    it 'title should be present' do
+      subject.title = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'title should not be too long' do
+      subject.title = 'a' * 251
+      expect(subject).to_not be_valid
+    end
+
+    it 'comments counter should be greater that 0' do
+      subject.comments_counter = -1
+      expect(subject).to_not be_valid
+    end
+
+    it 'comments counter should be interger' do
+      subject.comments_counter = 'zero'
+      expect(subject).to_not be_valid
+    end
+
+    it 'likes counter should be greater that 0' do
+      subject.likes_counter = -1
+      expect(subject).to_not be_valid
+    end
+
+    it 'likes counter should be interger' do
+      subject.likes_counter = 'zero'
+      expect(subject).to_not be_valid
+    end
+  end
+
+  describe 'should test recent comments method' do
+   
+    it 'recent comments should return five comments' do
+      expect(subject.recent_comments).to eq(subject.comments.last(5))
+    end
+  end
 end
